af7653f5fad6af920f24b89e9c8df7034e4d8a2a
ajay.sangana <44274006+Ajay396103@users.noreply.github.com>
2021-10-08 07:53:41 +0530

    Update Palindrome.java
    Removing loop traversals and using nCopies method for generating copies of distinct characters list

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,15 @@
+/**
+* @author Ajay Kumar
+*/
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+/*
+ * Print a palindrome string of length 'N' where N is a positive integer value range 1<=N<=2^32-1
+ * With K distinct values 1<=K<=26
+ */
 
 public class Palindrome {
     public static void main(String[] args)
@@ -10,13 +19,20 @@ public class Palindrome {
         int length=sc.nextInt();
         System.out.println("Please enter the length of the distinct characters needed");
         int distinct=sc.nextInt();
-        //Check for the possibility of palindrome of length 'length' with distinct 'distinct' characters
-       if(length>0 && distinct > 0 && length!=distinct)
+        
+        /*Check for the length of the palindrome possible with the distinct characters
+         * A palindrome is a string with same characters at position i and length n-i
+         * A maximum of N/2 characters if N is odd and (N/2 +1) if N is even distinct chars can be used to form a palindrome string
+         */
+
+       if(length>0 && distinct > 0 && length!=distinct && distinct<=26)
        {
            int palindromeLength=(length%2==0 ? length/2 : ((length/2)+1));
+           String result=new String();
            if(distinct<=palindromeLength)
            {
-               System.out.println("A palindrome string of length: "+ length + " with " + distinct + " distinct characters formed is : " + nPalindrome(length,distinct,palindromeLength)  );
+               result=nPalindrome(length,distinct,palindromeLength);
+               System.out.println("A palindrome string of length: "+ length + " with " + distinct + " distinct characters formed is : " + result  );
            }
            else
            {
@@ -25,12 +41,11 @@ public class Palindrome {
        }
        else
        {
-           System.out.println("A palindrome string of length: "+ length + " with " + distinct + " distinct characters cannot be formed"  );
+           System.out.println("A palindrome string of length: "+ length + " with " + distinct + " distinct characters cannot be formed as both are equal"  );
        }
-
-
     }
-    //Form a string based on length 'N'
+
+    // Fill the "N" length string with Character list of size N/2 (where N is even) or N/2 +1 (N is odd)
     private static String nPalindrome(int length,int distinct, int palindromeLength)
     {
         Character[] resultList= new Character[length];
@@ -38,46 +53,31 @@ public class Palindrome {
         ArrayList<Character> aList=palindromeString(distinctArray(distinct),palindromeLength);
             for (int i = 0 ; i < aList.size() ; i++ )
             {
-                if(i<(length-i-1))
+                if(i<=(length-i-1))
                 {
                     resultList[i]= aList.get(i);
                     resultList[length-i-1]=aList.get(i);
-                }
-                else if(i==(length-i-1))
-                {
-                    resultList[i]= aList.get(i);
                 }
             }
         return Arrays.asList(resultList).stream().map(i-> String.valueOf(i)).collect(Collectors.joining());
     }
 
+    // Generate  N/2 length arraylist if N is even else (N/2)+1 with distinct characters
     private static ArrayList palindromeString(ArrayList<Character> A,int palindromeLength)
     {
-
-        ArrayList<Character> aChar=new ArrayList<Character>();
-        if (A.size()==palindromeLength)
-        {
-            aChar.addAll(A);
-        }
-        else
-        {
-            while(aChar.size()<palindromeLength)
-            {
-                aChar.addAll(A);
-            }
-            System.out.println(aChar.toString());
-            IntStream.range(palindromeLength,aChar.size()).forEach(i->
-            {
-                aChar.remove(aChar.size()-1);
-            });
-        }
-        System.out.println(aChar.toString());
-        return aChar;
+        List<Character> aChar=new ArrayList<Character>();
+        aChar=  Collections.nCopies(
+                            ((palindromeLength%A.size())==0 ? (palindromeLength/A.size()) :(palindromeLength/A.size())+1),A)
+                            .stream()
+                            .flatMap(x -> x.stream())
+                            .limit(palindromeLength)
+                            .collect(Collectors.toList());
+        return  new ArrayList<Character> (aChar);
     }
-
+    
+    // Get an arrayList of K length with distinct characters
     private static ArrayList distinctArray(int distinct)
-    {
-        ArrayList<Character> array=new ArrayList<Character>();
+    {        ArrayList<Character> array=new ArrayList<Character>();
         IntStream.range(0,distinct).forEach(i->
         {
             array.add(returnChar(array));
@@ -85,6 +85,7 @@ public class Palindrome {
         return  array;
     }
 
+    // Generate and return a random non existing alphabet in ArrayList
     private static char returnChar(ArrayList<Character> A)
     {
         Random random = new Random();
